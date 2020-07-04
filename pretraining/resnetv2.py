f6d78b78160d6cf87df67e23a6a147f5c187005b
Tianlong Chen <wiwjp619@gmail.com>
2020-07-04 14:00:19 -0500

    type

--- pretraining/resnetv2.py
+++ pretraining/resnetv2.py
@@ -19,7 +19,7 @@ class Bottleneck(nn.Module):
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
